09a5ccb4339a168ac8ce5ebde19f4429c3a137a7
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-10-07 13:51:42 +0300

    error fix

--- client/src/components/Exercises.js
+++ client/src/components/Exercises.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import axios from 'axios';
+import React, {useState,useEffect} from 'react';
+import axios from '../api/axios';
 
 function Exercises(){
     const [exercises , setExercises] = useState([])
@@ -16,20 +16,21 @@ function Exercises(){
    
    return ( <>
 
-       <div className="container">
-
+     <div className="container">
+      <div className='crd-group'>
            {exercises.map((exercise ,index ) => (
-             <div key={exercise.id} className="card" style={{ width: "18rem" }}>
+             <div key={exercise.id} className="card" style={{ width: "18rem" , display: "flex" , flexDirection: "column" , flexFlow: "wrap"}}>
                 <div className="card-body">
-               <h1 className="card-title">{exercise.name}</h1>
-               <h2 className="card-text">{exercise.type}</h2>
-               <p className="card-text">{exercise.muscle}</p>
-               <p className="card-text">{exercise.equipment}</p>
-               <p className="card-text">{exercise.difficulty}</p>
-               <p className="card-text">{exercise.instructions}</p>
+               <h1 className="card-title">NAME: {exercise.name}</h1>
+               <h2 className="card-text">TYPE: {exercise.type}</h2>
+               <p className="card-text">MUSCLE: {exercise.muscle}</p>
+               <p className="card-text">EQUIPMENT: {exercise.equipment}</p>
+               <p className="card-text">DIFFICULTY: {exercise.difficulty}</p>
+               <button></button>
              </div>
            </div>))}
-       </div>
+           </div>
+       </div> 
    
    
    </> )
@@ -38,5 +39,3 @@ function Exercises(){
    
    
    export default Exercises
-
-
