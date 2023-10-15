59c5b625fb47fd2762ba1a6acbbca7b7be545606
hillaryke <tenskap@gmail.com>
2023-10-15 03:37:57 +0300

    use jwt create-access-token for auth

--- server/app.py
+++ server/app.py
@@ -4,13 +4,16 @@ from flask import Flask, make_response, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-
+from  werkzeug.security import generate_password_hash, check_password_hash
 from models import db, Exercise, Member
 
 from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
+
+from datetime import datetime, timedelta 
+from functools import wraps
 
 app = Flask(__name__)
 CORS(app)
@@ -47,7 +50,7 @@ def get_members():
     return jsonify(member_list)
 
 
-@app.route('/Signup', methods=['POST'])
+'''@app.route('/Signup', methods=['POST'])
 def Signup():
     data = request.get_json()
     if not data:
@@ -83,10 +86,101 @@ def Signin():
     db.session.add(member)
     db.session.commit()
 
-    return jsonify({"message": "Member logged in successfully"}), 201
+    return jsonify({"message": "Member logged in successfully"}), 201'''
    
    
+@app.route('/protected', methods=['GET'])
+class token_required():
+    @jwt_required()
+    def get(self):
+        # Access the identity of the current user with get_jwt_identity
+        current_user = get_jwt_identity()
+        return {'user_email': current_user}, 200
+
+
+@app.route('/members', methods =['GET'])
+# @token_required
+def get_all_users(current_user):
+    # querying the database
+    # for all the entries in it
+    users = Member.query.all()
+    # converting the query objects
+    # to list of jsons
+    output = []
+    for user in users:
+        # appending the user data json 
+        # to the response list
+        output.append({
+            'public_id': user.public_id,
+            'name' : user.name,
+            'email' : user.email
+        })
+  
+    return jsonify({'users': output})
+
+
+
+@app.route('/login', methods = ['POST'])
+def login():
+    auth = request.get_json()
+    print(auth)
+    if not auth or not auth.get('email') or not auth.get('password'):
+        return make_response(
+            'Could not verify', 401,
+            {'WWW-Authenticate': 'Basic realm="User does not exist!!"'}
+        )
+
+    member = Member.query\
+        .filter_by(email = auth.get('email'))\
+        .first()
+    #
+    #
+    if not member:
+        # returns 401 if user does not exist
+        return make_response(
+            'User does not exist',
+            401,
+            {'WWW-Authenticate' : 'Basic realm ="User does not exist !!"'}
+        )
+
+    if check_password_hash(member.password, auth.get('password')):
+        # generates the JWT Token
+        access_token = create_access_token(identity = auth.get('email'))
+        return make_response(jsonify({'token': access_token}), 201)
+
+    return make_response(
+        'Invalid password',
+        403,
+        {'WWW-Authenticate': 'Basic realm="Wrong Password !!"'}
+    )
     
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+
+    name, email = data.get('name'), data.get('email')
+    password = data.get('password')
+
+    member = Member.query\
+        .filter_by(email = email)\
+        .first()
+
+    if not member:
+        member = Member(
+            name=name,
+            email=email,
+            password=generate_password_hash(password)
+        )
+        db.session.add(member)
+        db.session.commit()
+
+        return make_response('Successfully registered.', 201)
+    else:
+        return make_response('User already exists. Please Log in', 202)
+
+    
+    
+
 
 # Define a route to retrieve a list of all exercises
 @app.route('/exercises', methods=['GET'])
